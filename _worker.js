cb26e41b4359a4263335bfc9ac8f50a834362c21
1hei1 <109926235+1hei1@users.noreply.github.com>
2024-05-29 18:50:04 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    url.host = "chatgpt.com";
+    url.host = "34.0.49.250:5005";
     // openai is already set all CORS heasders 
     return fetch(url, {
       headers: request.headers,
